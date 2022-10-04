f26f328be5ee88c3f217e6f3a1dab85f8de610d0
joao-s-rocha <joao.roha25@gmail.com>
2022-10-04 00:28:10 -0400

    lugar esperado para receber token

--- src/utils/validaLogin.ts
+++ src/utils/validaLogin.ts
@@ -4,8 +4,8 @@ import { CustomError } from "./customError";
 
 export class validaLogin implements ExpressMiddlewareInterface {
   async use(req: any, res: any, next: any) {
-    const tipo = req.headers.tipo;
-    const id = req.headers.id;
+    const tipo = req.headers.tokenCatequese.tipo;
+    const id = req.headers.tokenCatequese.id;
 
     if (isEmpty(tipo) || isEmpty(id))
       throw new CustomError(400, "Login incompleto");
