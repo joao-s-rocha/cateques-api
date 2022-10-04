5a031fb6579d51dd49ad8254cc0ed13263eb2711
João dos Santos Rocha <joao.santos@ganso.com.br>
2022-10-04 09:37:17 -0400

    retirando tipo_sacramento de turma

--- src/entities/turma.ts
+++ src/entities/turma.ts
@@ -12,16 +12,7 @@ import {
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
-  Unique,
 } from "typeorm";
-// import { Catequizando } from "./catequizando";
-// import { Catequizando } from "./catequizando";
-
-enum TipoSacramento {
-  EUCARISTIA = "E",
-  CRISMA = "C",
-  BATISMO = "B",
-}
 
 enum DiaSemana {
   DOMINGO = "DOMINGO",
@@ -40,13 +31,6 @@ export class Turma {
 
   @Column({ generatedType: "STORED", asExpression: `concat(dia_semana, hora)` })
   descricao!: string;
-
-  @Column({ type: "enum", enum: TipoSacramento, nullable: false })
-  @MaxLength(1, { message: "Tamanho máximo para esse campo é de 1 caracteres" })
-  @MinLength(1, { message: "Tamanho máximo para esse campo é de 1 caracteres" })
-  @IsEnum(TipoSacramento, { message: "Enum não correspondente" })
-  @IsNotEmpty()
-  tipo_sacramento!: string;
 
   @Column({ type: "enum", enum: DiaSemana, nullable: false })
   @MaxLength(8, { message: "Tamanho máximo para esse campo é de 8 caracteres" })
