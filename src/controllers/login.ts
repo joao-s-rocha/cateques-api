cf5d2f25f33256bcab2210a51d12b1ac0d130b89
joao-s-rocha <joao.roha25@gmail.com>
2022-10-12 12:04:06 -0400

    exemplo token

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -14,10 +14,8 @@ export class Login {
         content: {
           "application/json": {
             example: {
-              tokenCatequese: {
-                tipo: "XXXXX",
-                id: 1,
-              },
+              token:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
             },
           },
         },
